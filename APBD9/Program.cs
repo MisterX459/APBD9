4b5332deae0fe1553ed35f0fd3613b813bedb4d7
MisterX459 <65484118+MisterX459@users.noreply.github.com>
2024-06-04 00:48:37 +0200

    Commit

--- APBD9/Program.cs
+++ APBD9/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
+//commit
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
